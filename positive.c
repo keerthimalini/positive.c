e6632d01eaeb213f4776249277e1758d5be7506f
keerthimalini <43332204+keerthimalini@users.noreply.github.com>
2019-02-11 10:36:23 +0530

    Create positive.c

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+int A;
+  printf("Enter the number A");
+  scanf("%d"&a);
+  if (A>0):
+      printf ("positive");
+   elif (A=0):
+       printf("zero");
+    else:
+        printf("Negative");
+        return 0;
